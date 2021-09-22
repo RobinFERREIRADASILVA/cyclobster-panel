9c08a764effebcf26c48c43103585d193d77d6ac
RobinFERREIRADASILVA <ferreiradasilvarobin@gmail.com>
2021-09-22 19:48:42 +0200

    Buttons

--- src/main.js
+++ src/main.js
@@ -2,5 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import BalmUI from 'balm-ui'; // Official Google Material Components
+import BalmUIPlus from 'balm-ui-plus'; // BalmJS Team Material Components
+import 'balm-ui-css';
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App).use(store).use(router)
+
+app.use(BalmUI); // Mandatory
+app.use(BalmUIPlus); // Optional
+
+app.mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,46 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.jpg">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="buttons">
+      <ui-button raised>Gérez les photos</ui-button>
+          <router-link to="/about"><ui-button raised>Gérez les catégories</ui-button>
+</router-link>
+
+    </div>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
 
-<style>
+<style scoped>
+
+img {
+  max-width: 200px;
+}
+
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+}
+  .buttons {
+    display:flex;
+    justify-content: center;
+    margin-top: 10%;
+  }
+
+  .buttons :first-child {
+    margin-right: 3em;
+  }
 </style>
